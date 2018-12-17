62f6b73b1645a6157e54f7c95939bd1455f9e21d
william nico <>
2018-12-17 12:19:31 +0700

    service V 2.8

--- ServicePemesananGalonGas/src/java/Helper/StockbarangHelper.java
+++ ServicePemesananGalonGas/src/java/Helper/StockbarangHelper.java
@@ -4,3 +4,31 @@
  * and open the template in the editor.
  */
 
+package Helper;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import pojos.Pesanan;
+import pojos.Stockbarang;
+import util.NewHibernateUtil;
+
+/**
+ *
+ * @author ASUS
+ */
+public class StockbarangHelper {
+     public StockbarangHelper() {
+    }
+    public List<Stockbarang> getAllLaporan(){
+        List<Stockbarang> result = null;
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        String query = "from Stockbarang s";
+        Query q = session.createQuery(query);
+        result = q.list();
+        session.close();
+        return result;
+    }
+
+    
+}
